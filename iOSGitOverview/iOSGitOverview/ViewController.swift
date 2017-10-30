ba7d394664a3f46252d94ae21fda8f9e6ee106cc
Martin Meincke <meincke91@hotmail.com>
2017-10-30 09:07:45 +0100

    Initial project creation

---
+++ iOSGitOverview/iOSGitOverview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSGitOverview
+//
+//  Created by Martin Meincke on 30/10/2017.
+//  Copyright © 2017 Martin Meincke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
